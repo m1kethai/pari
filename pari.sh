3b5165d762351a06e44ff76ad5064951beab2feb
Mike Thai <miketh.ai@protonmail.com>
2023-08-29 14:27:47 -0400

    output styling/formatting improvements

--- pari.sh
+++ pari.sh
@@ -11,45 +11,61 @@ repository=$(echo "$pkg_info" | awk '/^Repository/ { print $3 }')
 # Determine the URL based on the repository
 case $repository in
     core)
-        repo_url="https://www.archlinux.org/packages/core/x86_64/$package_name/"
+        repo_url="https://www.archlinux.org/packages/core/x86_64/$package_name"
         ;;
     extra)
-        repo_url="https://www.archlinux.org/packages/extra/x86_64/$package_name/"
+        repo_url="https://www.archlinux.org/packages/extra/x86_64/$package_name"
         ;;
     community)
-        repo_url="https://www.archlinux.org/packages/community/x86_64/$package_name/"
+        repo_url="https://www.archlinux.org/packages/community/x86_64/$package_name"
         ;;
     aur)
-        repo_url="https://aur.archlinux.org/packages/$package_name/"
+        repo_url="https://aur.archlinux.org/packages/$package_name"
         ;;
     multilib)
-        repo_url="https://www.archlinux.org/packages/multilib/x86_64/$package_name/"
+        repo_url="https://www.archlinux.org/packages/multilib/x86_64/$package_name"
         ;;
     chaotic-aur)
-        repo_url="https://lonewolf.pedrohlc.com/chaotic-aur/x86_64/$package_name/"
+        repo_url="https://lonewolf.pedrohlc.com/chaotic-aur/x86_64/$package_name"
         ;;
     blackarch)
-        repo_url="https://www.blackarch.org/packages/$package_name/"
+        repo_url="https://www.blackarch.org/packages/$package_name"
         ;;
     *)
         repo_url="https://www.archlinux.org/packages/?q=$package_name"
         ;;
 esac
 
-# Responsive divider
-function divi {
-    divi_str=""
-    divi_segment="#"
-    divi_length=$(( $(tput cols) < ${#repo_url} + 15 ? $(tput cols) : ${#repo_url} + 15 ))
+# Prints a pink responsive divider
+function print_divider {
+    local term_width="$(tput cols)"
 
-    for ((i = 1; i <= divi_length; i++)); do
-        divi_str+=$divi_segment
+    local divider=""
+    local divi_length=$(( $term_width ))
+    local divi_segment="#"
+
+    for ((i=1; i<=divi_length; i++)); do
+        divider+=$divi_segment
     done
 
-    echo -e "\n$divi_str\n"
+    echo -e "\e[95m$divider\e[0m"
 }
 
-# Append the URL to the bottom of the pkg info output
-echo "$pkg_info"
-divi
-echo -e "🔗 $repo_url"
+# Prints the package information wrapped in dividers
+function print_pkg_info {
+    echo -e ""
+    print_divider
+    echo "$pkg_info"
+    print_divider
+    echo -e ""
+}
+
+# Styles the repo url with bold, underline, italic and cyan text
+function print_styled_url {
+    echo -e "🔗  \e[1m\e[4m\e[3m\e[94m$repo_url\e[0m"
+}
+
+echo -e ""
+echo -e "ℹ️   \e[1m\e[4m\e[32m$(echo $package_name | tr '[:lower:]' '[:upper:]')\e[0m"
+print_pkg_info
+print_styled_url
